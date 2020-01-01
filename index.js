71e22919153f3ee8404ea4bd316f32cc34f904ee
purplnay <nay@purpltag.com>
2020-01-01 22:24:53 +0100

    Improve update method

--- index.js
+++ index.js
@@ -7,8 +7,8 @@ class MarkovChain {
    * Create a Markov chain.
    * 
    * @param {number} [nGrams=3] The size of the n-gram.
-   * @param {string} [start=%startf%] The start delimiter to use. Must not contain the separation token.
-   * @param {string} [end=%endf%] The end delimiter to use. Must not contain the separation token.
+   * @param {string} [start='%startf%'] The start delimiter to use. Must not contain the separation token.
+   * @param {string} [end='%endf%'] The end delimiter to use. Must not contain the separation token.
    * @param {string} [separation=' '] The separation token to use to split the new texts.
    */
   constructor(nGrams = 3, start = '%startf%', end = '%endf%', separation = ' ') {
@@ -24,17 +24,15 @@ class MarkovChain {
      * The formatted start delimiter.
      * 
      * @type {string}
-     * @private
      */
-    this._start = `${start} `;
+    this.start = start;
 
     /**
      * The formatted end delimiter.
      * 
      * @type {string}
-     * @private
      */
-    this._end = ` ${end}`;
+    this.end = end;
 
     /**
      * The separation token used to split the new texts.
@@ -52,26 +50,6 @@ class MarkovChain {
   }
 
   /**
-   * The start delimiter.
-   * 
-   * @readonly
-   * @type {string}
-   */
-  get start() {
-    return this._start.trim();
-  }
-
-  /**
-   * The end delimiter.
-   * 
-   * @readonly
-   * @type {string}
-   */
-  get end() {
-    return this._end.trim();
-  }
-
-  /**
    * Update the corpus of the chain.
    * 
    * @param {string} text The text to add.
@@ -80,12 +58,12 @@ class MarkovChain {
   update(text) {
     if (text.trim().length === 0) return this;
 
-    const words = this._trim(`${this._start.repeat(this.nGrams)} ${text} ${this._end.repeat(this.nGrams)}`).split(this.separation);
+    const words = [this.start, ...`${text}`.split(this.separation), this.end];
 
     for (let i in words) {
       const index = Number(i);
 
-      if (index < words.length - (this.nGrams - 1)) {
+      if (index < words.length - 1) {
         this.corpus.push(words.slice(index, index + this.nGrams));
       }
     }
@@ -96,36 +74,25 @@ class MarkovChain {
   /**
    * Generate a new text.
    * 
-   * @param {string} [start=%this._start%] The word to start with.
+   * @param {string} [start=this.start] The word to start with.
    * @return {string} The generated text.
    */
-  generate(start = this._start) {
+  generate(start = this.start) {
     if (this.corpus.length === 0) return '';
 
-    const words = [start.trim()];
+    const words = [start];
 
-    while (words[words.length - 1] !== this._end.trim()) {
+    while (words[words.length - 1] !== this.end) {
       const nextChains = this.corpus.filter(chain => chain[0] === words[words.length - 1]);
       const nextChain = nextChains[Math.floor(Math.random() * nextChains.length)];
-
+console.log(nextChain)
       words.push(...nextChain.slice(1));
     }
 
-    return words.join(this.separation)
-      .replace(new RegExp(this._start, 'g'), '')
-      .replace(new RegExp(this._end, 'g'), '')
-      .trim();
-  }
+    words.shift();
+    words.pop();
 
-  /**
-   * Removes all double+ spaces and leading and trailing spaces.
-   * 
-   * @param {string} str The string to trim.
-   * @return {string} The trimmed string.
-   * @private
-   */
-  _trim(str) {
-    return str.replace(/\s\s+/g, ' ').trim();
+    return words.join(this.separation);
   }
 
   /**
@@ -158,4 +125,8 @@ class MarkovChain {
   }
 }
 
-module.exports = MarkovChain;
+if (typeof window !== 'undefined') {
+  window.MarkovChain = MarkovChain;
+} else {
+  module.exports = MarkovChain;
+}
